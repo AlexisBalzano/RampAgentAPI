d7368d3ac4ccf0ba6fa1bdaaf5b73c9abbd34484
Alexis <alexis.balzano@epfl.ch>
2025-10-05 11:50:39 +0200

    Added initial logging test

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,10 +1,13 @@
 const occupancyService = require('../services/occupancyService');
+const { info } = require('../utils/logger');
 
 exports.handleReport = (req, res) => {
   const { callsign, occupied } = req.body;
   if (!callsign || !Array.isArray(occupied)) {
     return res.status(400).json({ error: 'Invalid payload' });
   }
+
+  info(`Received report for ${callsign}: ${occupied.length} occupied spots`);
 
   occupancyService.updateClientReport(callsign, occupied);
   res.json({ status: 'ok' });
