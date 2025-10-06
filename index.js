c771fb8d96edfe2cba73f7c012c94f92905bcafc
Alexis <alexis.balzano@epfl.ch>
2025-10-06 19:28:59 +0200

    Added html viewer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const env = process.env.NODE_ENV || 'default';
 const config = require(`./config/${env}.js`);
@@ -9,7 +10,13 @@ const logger = require('./utils/logger');
 const app = express();
 app.use(express.json());
 
+// Serve viewer
+app.get('/debug', (req, res) => {
+  res.sendFile(path.join(__dirname, 'viewer', 'viewer.html'));
+});
+
 // Register routes
+app.use('/debug', express.static(path.join(__dirname, 'viewer')));
 app.use('/report', reportRoutes);
 app.use('/assign', assignRoutes);
 
