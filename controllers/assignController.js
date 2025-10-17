b3583310e09a7e0cb5c605ff1fe30764d553f106
Alexis <alexis.balzano@epfl.ch>
2025-10-17 22:40:46 +0200

    fixed manual stand assignement

--- controllers/assignController.js
+++ controllers/assignController.js
@@ -2,10 +2,10 @@ const occupancyService = require('../services/occupancyService');
 const stat = require('../services/statService');
 
 exports.assignStand = (req, res) => {
-    const { standName, icao, callsign } = req.query;
+    const { stand, icao, callsign } = req.query;
     try {
         stat.incrementRequestCount();
-        const result = occupancyService.assignStandToPilot(standName, icao, callsign);
+        const result = occupancyService.assignStandToPilot(stand, icao, callsign);
         res.json({ success: true, message: result });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
