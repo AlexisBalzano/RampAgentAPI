94ec320f38c30c009b4b717ed9fdc8688cdce67a
Alexis <alexis.balzano@epfl.ch>
2025-10-29 10:00:54 +0100

    Added Crypto import

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,13 @@
+const crypto = require('crypto');
+
 exports.verifyToken = (token, cid, client) => {
   // Return true if token is valid, false otherwise
   const secret = process.env.AUTH_SECRET;
   
   //FIXME: add to secret
-  // if (!secret) {
-  //   return false;
-  // }
+  if (!secret) {
+    return false;
+  }
 
   const expectedToken = crypto.createHash('sha256').update(secret + cid + client).digest('hex');
   return token === expectedToken;
