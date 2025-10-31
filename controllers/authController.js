136f9d024ab83ae6f99804a8161cf913189e8d77
Alexis <alexis.balzano@epfl.ch>
2025-10-31 12:45:15 +0100

    removed comment

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
+const { error } = require('console');
 const crypto = require('crypto');
 
 exports.verifyToken = (token, cid, client) => {
   // Return true if token is valid, false otherwise
   const secret = process.env.AUTH_SECRET;
   
-  //FIXME: add to secret
   if (!secret) {
     return false;
   }
