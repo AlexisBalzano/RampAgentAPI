1c81367a07cf4eb6900b0aa805c6252dec4ac57e
Alexis <alexis.balzano@epfl.ch>
2025-10-31 14:00:02 +0100

    Added token verification for manual stand assignation

--- controllers/assignController.js
+++ controllers/assignController.js
@@ -1,8 +1,18 @@
 const occupancyService = require('../services/occupancyService');
 const stat = require('../services/statService');
+const { verifyToken } = require('./authController');
 
 exports.assignStand = async (req, res) => {
-    const { stand, icao, callsign } = req.query;
+    const { stand, icao, callsign, token, client, cid } = req.query;
+
+    if (!stand || !icao || !callsign || !token || !client || !cid) {
+        return res.status(400).json({ success: false, message: 'Missing required parameters' });
+    }
+
+    if (verifyToken(token, client, cid) === false) {
+        return res.status(403).json({ success: false, message: 'Invalid token' });
+    }
+
     try {
         stat.incrementRequestCount();
         const result = await occupancyService.assignStandToPilot(stand, icao, callsign);
