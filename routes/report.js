02f5315abf92f3a53eca49d55c668dca713d38b3
Alexis <alexis.balzano@epfl.ch>
2025-11-01 10:01:43 +0100

    Deletion of unused

--- routes/report.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-router.post('/', reportController.handleReport);
-
-module.exports = router;
