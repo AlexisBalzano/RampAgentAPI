d81778bd50fe3fe36b348a33d18bb820607ce118
Alexis <alexis.balzano@epfl.ch>
2025-11-09 16:02:29 +0100

    Added header for core request

--- controllers/authController.js
+++ controllers/authController.js
@@ -139,6 +139,8 @@ exports.login = (req, res) => {
   params.set("origin", baseURL)
   params.set("redirect", `${baseURL}/api/auth/callback`)
   const loginUrl = `${process.env.CORE_URL_EXTERNAL}/v1/auth/vatsim/login?` + params.toString();
+
+  res.setHeader('Referer', 'https://pintade.vatsim.fr/');
   return res.redirect(loginUrl);
 };
 
